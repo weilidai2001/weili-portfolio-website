dbbe5a982cf3069d10ed5321ad7639db8b2d21b8
weili.dai <weili.dai@gumtree.com>
2025-06-16 15:38:32 +0100

    Try adding prefix for GitHub pages route

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = 'weili-portfolio-website';
+
 const nextConfig: NextConfig = {
   distDir: 'docs',
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
